cf78ad29223b8b9e413c51b97abc68f5a5e541c5
André Silva <77990817+andresilva16@users.noreply.github.com>
2022-03-14 10:29:06 -0300

    first commit

---
+++ pages/home.php
@@ -0,0 +1,126 @@
+<section class="banner-container">
+	<div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/bg-banner.jpg');" class="banner-single"></div><!--banner-single-->
+	<div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/bg-cat1.jpg');" class="banner-single"></div><!--banner-single-->
+	<div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/bg-cat3.jpg');" class="banner-single"></div><!--banner-single-->
+		<div class="overlay"></div><!--overlay--> 
+		<div class="center">
+			<form>  
+				<h2>Qual o seu melhor email?</h2> 
+				<input type="email" name="email">
+				<input type="submit" name="acao" value="Cadastrar">
+			</form>
+		</div><!--center--> 
+		<div class="bullets"></div><!--bullets--> 
+	</section><!--banner-pricipal-->
+
+	<section class="descricao-autor">
+		<div class="center">
+			<div class="w50 left">
+				<h2>Guilherme Grillo</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div><!--w50--> 
+			<div class="w50 left">
+				<img class="right" src="<?php echo INCLUDE_PATH; ?>images/foto.jpg">
+			</div><!--w50-->
+			<div class="clear"></div>
+		</div>
+	</section>
+
+<section class="especialidades">
+	
+	<div class="center">
+		<h2 class="title">Especialidades</h2>
+			<div class="w33 left box-especialidade">
+				<h3><i class="fa fa-css3" aria-hidden="true"></i></h3>
+				<h4>CSS3</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+					sed do eiusmod,Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod, </p>
+			</div><!--box-especialidade-->
+
+			<div class="w33 left box-especialidade">
+				<h3><i class="fa fa-html5" aria-hidden="true"></i></h3>
+				<h4>HTML 5</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod, 
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod,
+				</p>
+			</div><!--box-especialidade-->
+
+			<div class="w33 left box-especialidade">
+				<h3><i class="fa fa-gg-circle" aria-hidden="true"></i></h3>
+				<h4>Javascript</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod,
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod,
+				</p>
+			</div><!--box-especialidade-->
+			<div class="clear"></div>
+		</div><!--center-->
+	</section><!--especialidades--> 
+
+	<section class="extras">
+
+		<div class="center">
+			<div id="depoimentos" class="w50 left depoimentos-container">
+				<h2 class="title">Depoimentos dos nossos clientes</h2>
+				<div class="depoimento-single">
+				<div class="depoimento-single">
+					<p class="depoimento-descricao">
+						"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+					<p class="nome-autor">Lorem ipsum</p>
+				</div><!--depoimento-single-->
+				<div class="depoimento-single">
+					<p class="depoimento-descricao">
+						"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+					<p class="nome-autor">Lorem ipsum</p>
+				</div><!--depoimento-single-->
+				<div class="depoimento-single">
+					<p class="depoimento-descricao">
+						"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+					<p class="nome-autor">Lorem ipsum</p>
+				</div><!--depoimento-single-->
+				</div><!--depoimento-single-->
+			</div><!--w50-->
+
+			<div id="servicos" class="w50 left servicos-container">
+				<h2 class="title">Serviços</h2>
+				<div class="servicos">
+					<ul>
+						<li>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						 sunt in culpa qui officia deserunt mollit anim id est laborum."</li>
+						<li>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						 sunt in culpa qui officia deserunt mollit anim id est laborum."</li>
+						<li>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						 sunt in culpa qui officia deserunt mollit anim id est laborum."</li>
+					</ul>
+				</div><!--servicos-->
+			</div><!--w50-->
+			<div class="clear"></div>
+		</div><!--center-->
+	</section><!--extras--> 
